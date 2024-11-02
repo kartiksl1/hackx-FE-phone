5666d424d7d80aab1c66e74259b93af1827cdf25
kartik <kartik@silencelaboratories.com>
2024-11-02 22:52:03 +0530

    added top bar

---
+++ src/app/signaturepage/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+import Top from "@/public/Top app bar.svg"
+
+function Page() {
+  return (
+    <div>
+        <Image src={Top} alt='' />
+
+    </div>
+  )
+}
+
+export default Page
--- src/components/PhoneScreen.tsx
+++ src/components/PhoneScreen.tsx
@@ -7,7 +7,7 @@ interface PhoneScreenProps {
 
 const PhoneScreen: React.FC<PhoneScreenProps> = ({ children }) => {
   return (
-    <div className="w-[393px] h-[853px] bg-white rounded-2xl shadow-lg overflow-hidden">
+    <div className="w-[360px] h-[720px] bg-white rounded-2xl shadow-lg overflow-hidden">
       {children}
     </div>
   );
